dc8404adfa7c2b638d2dbad41374348a6bb3c152
RaghavendraSR06 <raghu99459@gmail.com>
2021-06-20 13:06:51 +0530

    Add files via upload

---
+++ Receiver/BMSReceiverTests/StreamDataReceiverTests.cs
@@ -0,0 +1,33 @@
+﻿using BMSReceiver;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BMSReceiverTests
+{
+    [TestClass]
+    public class StreamDataReceiverTests
+    {
+        [TestMethod]
+        public void GivenBatteryInput_WhenBatteryDataInputIsNull_ThenReturnNull()
+        {
+            IStreamDataReceiver receiver = new StreamDataReceiver();
+            var result = receiver.GetDataFromReceiver(null);
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void GivenBatteryInput_WhenBatteryDataInputIsEmpty_ThenReturnNull()
+        {
+            IStreamDataReceiver receiver = new StreamDataReceiver();
+            var result = receiver.GetDataFromReceiver("");
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void GivenBatteryInput_WhenBatteryDataInputIsValid_ThenReturnInstanceOfBatteryProperties()
+        {
+            IStreamDataReceiver receiver = new StreamDataReceiver();           
+            var result = receiver.GetDataFromReceiver("23,12");
+            Assert.IsInstanceOfType(result,typeof(BatteryProperties));
+        }
+    }
+}
